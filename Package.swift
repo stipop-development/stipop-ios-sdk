ef66c388990414644f48d812ee5385017443280e
JayHarrisonAhn <jay@stipop.io>
2021-12-16 18:49:29 +0900

    0.3.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Stipop",
-            url: "https://ios-sdk.stipop.com/0.3.0-beta.3/Stipop.xcframework.zip",
-            checksum: "6f2216c30b2406ada6f4fb002dcb5a703be37bb31878fbbe38ac036fd6a17fc5"
+            url: "https://ios-sdk.stipop.com/0.3.0/Stipop.xcframework.zip",
+            checksum: "61f84cdd556a6113404e4f84c3be2b07c912fe8ef9f05c813844704683a65bb3"
         )
     ]
 )
