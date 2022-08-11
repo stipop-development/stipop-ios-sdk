dab966f08217ac7ed54c9d0b1d1dfd2e0a1250b5
KimIlKyum <kimilkyum.developer@gmail.com>
2022-08-11 17:46:01 +0900

    0.9.2

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Stipop",
-            url: "https://ios-sdk.stipop.com/0.9.1-beta.1/Stipop.xcframework.zip",
-            checksum: "7ee10ace7ee439d15f65c0fa0d2c102b75b2bc15c5eb8064026f12c98b9b239f"
+            url: "https://ios-sdk.stipop.com/0.9.2/Stipop.xcframework.zip",
+            checksum: "310b6aca34f4589ea75e3ab7e215ab2e4cd3e5f98c0e6475553d22287a73e48e"
         )
     ]
 )
